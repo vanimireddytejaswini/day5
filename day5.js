5963d3e6f82afda45051a256d74f7f151d27890f
vanimireddytejaswini <115021039+vanimireddytejaswini@users.noreply.github.com>
2024-02-16 20:54:45 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll('#carousel img');
+const pagination = document.querySelector('#pagination');
+
+function createPaginationMarkers() {
+  images.forEach((img) => {
+    const imgViewName = `--${img.id}`;
+    img.style.viewTimelineName = imgViewName;
+    const marker = document.createElement('button');
+    marker.type = 'button';
+    marker.role = 'tab';
+    marker.style.animationTimeline = imgViewName;
+    marker.addEventListener('click', () => img.scrollIntoView());
+    pagination.appendChild(marker);
+  });
+
+  document.body.style.timelineScope = `${Array.from(images).map(
+    (image) => image.style.viewTimelineName
+  )}`;
+}
+
+// Check browser support for Scroll-driven Animations
+if (CSS.supports('view-timeline-axis', 'inline')) {
+  createPaginationMarkers();
+}
+
+// Start scrolling from the second image
+images[1].scrollIntoView();
